b5471df87ac3006e06d8811229d8dc5bd7d0e1fc
Eason Chai <echai2905@gmail.com>
2023-05-23 19:09:15 +0800

    feat: Add bigger map and boundary

--- packages/client/src/layers/phaser/createPhaserLayer.ts
+++ packages/client/src/layers/phaser/createPhaserLayer.ts
@@ -1,21 +1,42 @@
 import { createPhaserEngine } from "@latticexyz/phaserx";
-import { namespaceWorld } from "@latticexyz/recs";
+import { useComponentValue } from "@latticexyz/react";
+import { namespaceWorld, getComponentValueStrict } from "@latticexyz/recs";
 import { NetworkLayer } from "../network/createNetworkLayer";
 import { registerSystems } from "./systems";
+import { TILE_HEIGHT, TILE_WIDTH } from "./constants";
 
 export type PhaserLayer = Awaited<ReturnType<typeof createPhaserLayer>>;
 type PhaserEngineConfig = Parameters<typeof createPhaserEngine>[0];
 
-export const createPhaserLayer = async (networkLayer: NetworkLayer, phaserConfig: PhaserEngineConfig) => {
+export const createPhaserLayer = async (
+  networkLayer: NetworkLayer,
+  phaserConfig: PhaserEngineConfig
+) => {
   const world = namespaceWorld(networkLayer.world, "phaser");
 
-  const { game, scenes, dispose: disposePhaser } = await createPhaserEngine(phaserConfig);
+  const {
+    game,
+    scenes,
+    dispose: disposePhaser,
+  } = await createPhaserEngine(phaserConfig);
   world.registerDisposer(disposePhaser);
+
+  const mapConfig = getComponentValueStrict(
+    networkLayer.components.MapConfig,
+    networkLayer.singletonEntity
+  );
 
   const { camera } = scenes.Main;
 
-  camera.phaserCamera.setBounds(-1000, -1000, 2000, 2000);
-  camera.phaserCamera.centerOn(0, 0);
+  const width = mapConfig.width;
+  const height = mapConfig.height;
+
+  console.log(0, 0, width * TILE_WIDTH, height * TILE_HEIGHT, "help");
+  camera.phaserCamera.setBounds(0, 0, width * TILE_WIDTH, height * TILE_HEIGHT);
+  camera.phaserCamera.centerOn(
+    (width / 2) * TILE_WIDTH,
+    (height / 2) * TILE_HEIGHT
+  );
 
   const components = {};
 
@@ -25,6 +46,7 @@ export const createPhaserLayer = async (networkLayer: NetworkLayer, phaserConfig
     game,
     scenes,
     components,
+    mapConfig,
   };
 
   registerSystems(layer);
---
+++ packages/client/src/mud/types.ts
@@ -0,0 +1,21 @@
+export enum ResourceType {
+  Wood,
+  Stone,
+  Water,
+}
+
+export enum ItemType {
+  Axe,
+  Pickaxe,
+  Bucket,
+}
+
+export enum TerrainType {
+  Tree = 1,
+  Rock,
+  Sea,
+  Wood,
+  Stone,
+  Water,
+  Food,
+}
